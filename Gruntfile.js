503517dc37bc75e08d2abf12a67c89590cbd85e9
Chicker <vadim.agishev@gmail.com>
2013-10-21 15:57:31 +0600

    Добавил Grunt задачу по автоматическому выполнению тестов при изменении исходных файлов.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,38 @@
 module.exports = function (grunt) {
-  "use strict";
-  grunt.initConfig({
-    watch: {
-      requirejs: {
-        files: ["lib/**/*.js"],
-        tasks: ['bgShell:requirejs']
-      }
-    },
+    "use strict";
+    grunt.initConfig({
+        watch: {
+            requirejs: {
+                files: ["lib/**/*.js"],
+                tasks: ['bgShell:requirejs']
+            },
+            "mochatest": {
+               files: ["test/**/*.js"],
+               tasks: ["bgShell:mocha-phantomjs"]
+            }
+        },
 
-    bgShell: {
-      requirejs: {
-        cmd: 'r.js -convert lib/ build/',
-        stdout: true,
-        stderr: true,
-        bg: false
-      },
+        bgShell: {
+            requirejs: {
+                cmd: 'r.js -convert lib/ build/',
+                stdout: true,
+                stderr: true,
+                bg: false
+            },
 
-      'mocha-phantomjs': {
-        cmd: 'mocha-phantomjs -R dot TestRunner.html',
-        stdout: true,
-        stderr: true,
-        bg:false
-      }
-    }
-
+            'mocha-phantomjs': {
+                cmd: 'mocha-phantomjs TestRunner.html',
+                stdout: true,
+                stderr: true,
+                bg:false
+            }
+        }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-bg-shell');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-bg-shell');
 
-  grunt.registerTask('build', ['bgShell:requirejs']);
-  grunt.registerTask("test", ["bgShell:requirejs", "bgShell:mocha-phantomjs"]);
+    grunt.registerTask('build', ['bgShell:requirejs']);
+    grunt.registerTask("test", ["bgShell:requirejs", "bgShell:mocha-phantomjs"]);
+    grunt.registerTask("autotest", ["bgShell:requirejs", "bgShell:mocha-phantomjs", "watch"]);
 };
